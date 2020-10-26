ff79b65440ee76bccfa7dac1b908a44c9a6a575d
Trịnh Xuân Đinh <37606430+trinhdinh0210@users.noreply.github.com>
2020-10-26 20:21:34 +0700

    dele

--- src/main/java/com/example/springmongodb/api/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springmongodb.api.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.springmongodb.api.model.User;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-
-}
